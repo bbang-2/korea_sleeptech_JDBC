6e0a9ffdb0e0b26db4abddeb56426162187ce393
bbang-2 <yys799@nate.com>
2025-04-23 15:12:22 +0900

    20250423

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,15 +1,36 @@
 package main.java;
 
-import main.java.manger.MemberManager;
+/*
+    1. 회원 CRUD 프로젝트 MemberManager
+        - dao: MemberDao
+        - db: DBConnection
+        - entity: Member
+        - manager: MemberManager
 
-import java.sql.SQLException;
+    2. 게시판 CRUD 프로젝트 (+ MVC 패턴)
+        - dao: PostDao
+        - db: DBConnection
+        - entity: Post
+        - service (비즈니스 로직 처리): PostService
+        - view: PostView
+        - manager: PostManager
+*/
+
+import main.java.manger.PostManager;
 
 public class Main {
     public static void main(String[] args) {
-        MemberManager mm = new MemberManager();
+//        MemberManager mm = new MemberManager();
+//        try {
+//            mm.run();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        PostManager pm = new PostManager();
         try {
-            mm.run();
-        } catch (SQLException e) {
+            pm.run();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
