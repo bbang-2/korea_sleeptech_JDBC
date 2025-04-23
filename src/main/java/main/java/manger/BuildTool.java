f90806ef5c5a1a274ae7c3207b4da2c68e3472ed
bbang-2 <yys799@nate.com>
2025-04-23 09:22:15 +0900

    20250422

--- src/main/java/main/java/manger/BuildTool.java
+++ src/main/java/main/java/manger/BuildTool.java
@@ -4,12 +4,13 @@ public class BuildTool {
     /*
         === 자바 빌드 도구 (Gradle Vs Maven) ===
 
-        1) Gradle
+        1) Gradle (빌드 스크립트 형식: )
         - 2007년 출시
         - Groovy / Kotlin DSL 기반 언어
         : 선언 + 명령형 방식
+        : 매우유연 + 
 
-        2) Maven
+        2) Maven (빌드 스크립트 형식: )
         - 2004년 출시
         - XML 기반 언어
         : 완전 선언형 방식
